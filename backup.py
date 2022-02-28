11e8647003b4157c493f26e84057c9ff2c884993
davevw21 <97575884+davevw21@users.noreply.github.com>
2022-02-28 00:59:55 -0500

    Update backup.py

--- backup.py
+++ backup.py
@@ -10,17 +10,16 @@ from getpass import getpass
 from pathlib import Path
 import getpass
 name = input("Please type your username: ")
-SDNstudent
+
 identification = getpass.getpass(prompt="Please enter your password: ")
-Python123
 
 directory = Path.cwd()
 backfile = 'csrBackup.txt'
 q = datetime.datetime.now
 write = "w"
-f = open(backfile, 'x')
+f = open(backfile, write)
 addy = '192.168.157.11'
-vrouter = {ip: addy, username: name, password: identification, device_type: 'cisco_ios'}
+vrouter = {'ip': addy, 'username': name, 'password': identification, 'device_type': 'cisco_ios'}
 connection = ConnectHandler(**vrouter)
 output = netmiko_send_command('showrun')
 f.write(output)
