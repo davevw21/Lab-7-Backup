a4c7af08b78aa9fca4d953970c8c1b01b6f71ccd
davevw21 <vanwdw@students.fscj.edu>
2022-03-12 20:03:54 -0800

    Added exceptions and error handling.

--- backup.py
+++ backup.py
@@ -9,6 +9,8 @@ from netmiko import ConnectHandler
 from getpass import getpass
 from pathlib import Path
 import getpass
+from netmiko.ssh_exception import AuthenticationException, SSHException, NetMikoTimeoutException
+
 name = input("Please type your username: ")
 
 identification = getpass.getpass(prompt="Please enter your password: ")
@@ -20,7 +22,15 @@ write = "w"
 f = open(backfile, write)
 addy = '192.168.157.11'
 vrouter = {'ip': addy, 'username': name, 'password': identification, 'device_type': 'cisco_ios'}
-connection = ConnectHandler(**vrouter)
-output = connection.send_command('show run')
-f.write(output)
-f.close() 
+
+try:
+    connection = ConnectHandler(**vrouter)
+    output = connection.send_command('show run')
+    f.write(output)
+    f.close()
+except (AuthenticationException):
+    print("An authentication error has occored while connecting to: " + vrouter['ip'])
+except (SSHException):
+    print("An error has occured while connecting to device " + vrouter['ip'] + " via ssh.  Ensure SSH is enabled.")
+except (NetMikoTimeoutException):
+    print("The device " + vrouter['ip'] + "has timed out while attempting to connect")
